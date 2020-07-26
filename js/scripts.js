713c3880522c48d8ede1def9dd965c9de52f0b4d
Christine <christineonaya5@gmail.com>
2020-07-26 21:24:47 +0300

    Add image width

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,18 @@ $(document).ready(function() {
         $("p.DESIGN").toggle();
     });
 });
+$(document).ready(function() {
+    $(".development").click(function() {
+        $("p.DEVELOPMENT").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".product-management").click(function() {
+        $("p.PRODUCT-MANAGEMENT").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".work4").hover(function() {
+
+    });
+});

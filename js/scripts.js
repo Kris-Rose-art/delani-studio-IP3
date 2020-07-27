6879911b1198b1069d3cf7fb72af0c6d96c2af6f
Christine <christineonaya5@gmail.com>
2020-07-27 07:07:35 +0300

    Add social icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
     $(".design").click(function() {
-        $("p.DESIGN").toggle();
+        $(".hide").toggle();
     });
 });
 $(document).ready(function() {
     $(".development").click(function() {
-        $("p.DEVELOPMENT").toggle();
+        $(".hide").toggle();
     });
 });
 $(document).ready(function() {
     $(".product-management").click(function() {
-        $("p.PRODUCT-MANAGEMENT").toggle();
+        $(".hide").toggle();
     });
 });
 $(document).ready(function() {
     $(".work4").hover(function() {
-
+        $(".work4").fadeout();
     });
 });
+$

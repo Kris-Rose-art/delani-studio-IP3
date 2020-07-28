4a7c5b6a4281a1f5c255ee85de0046bdd3eb2069
Christine <christineonaya5@gmail.com>
2020-07-28 08:49:37 +0300

    Add headers

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,53 @@
+// Adding the toggle effect.
 $(document).ready(function() {
-    $(".design").click(function() {
-        $(".hide").toggle();
+    $(".icon2").click(function() {
+        $("p.DEVELOPMENT").fadeToggle();
+        $(".icon2").hide();
+    });
+    $("p.DEVELOPMENT").click(function() {
+        $(".icon2").fadeToggle();
+        $("p.DEVELOPMENT").hide();
     });
 });
 $(document).ready(function() {
-    $(".development").click(function() {
-        $(".hide").toggle();
+    $(".icon3").click(function() {
+        $("p.PRODUCT-MANAGEMENT").fadeToggle();
+        $(".icon3").hide();
+    });
+    $("p.PRODUCT-MANAGEMENT").click(function() {
+        $(".icon3").fadeToggle();
+        $("p.PRODUCT-MANAGEMENT").hide();
     });
 });
 $(document).ready(function() {
-    $(".product-management").click(function() {
-        $(".hide").toggle();
+    $(".icon1").click(function() {
+        $("p.DESIGN").fadeToggle();
+        $(".icon1").hide();
+    });
+    $("p.DESIGN").click(function() {
+        $(".icon1").fadeToggle();
+        $("p.DESIGN").hide();
     });
 });
+// working the form
+
+function submitForm(event) {
+
+    var form = new FormData(event.target);
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var comment = $("textarea#message").val();
+        if($("input#name").val() && $("input#email").val()) {
+            alert(name +" "+ "we have received your message. Thankyou for reaching out to us");
+        }else {
+            alert("please enter your name and email");
+        }
+
+        event.preventDefault();
+} 
+// Adding the hover effect.
 $(document).ready(function() {
     $(".work4").hover(function() {
-        $(".work4").fadeout();
-    });
-});
-$
+        $(".ptag").fadeToggle(2500);
+    })
+})
